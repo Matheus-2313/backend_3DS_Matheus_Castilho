6f6fd3dd760ff7e01092263cd3ef1873759067db
Matheus-2313 <matheus.gomes.castilho@escola.pr.gov.br>
2026-04-08 12:37:38 +0000

    contiacao

--- funcoes/func-04-retorno.js
+++ funcoes/func-04-retorno.js
@@ -2,7 +2,8 @@ function sobradosalario(meuSalario){
     const contaDeluz = 150.0
     const aluguel = 1500.0
     const resto = meuSalario - (contaDeluz+aluguel) 
-    return `O que restou no fim do mês ${resto}`
+    
+    return `O que restou no fim do mês R$ ${resto.toFixed(2).replace('.',',')}`
 }
 const sobra = sobradosalario(1700.0);
 console.log(sobradosalario(20000.00))
