677b0d86fb76390dd4870e157fa492a21afaf11a
Matheus-2313 <matheus.gomes.castilho@escola.pr.gov.br>
2026-05-13 11:37:39 +0000

    gato amu

---
+++ 2 TRIMESTRE/modulo/mod02-ESM/main-mod-gatos.js
@@ -0,0 +1,11 @@
+import {banhoPet, tosaPet, alimentacaoPet} from './mod-gatos.js';
+
+let racaViraLata = 'Vira-Lata'
+
+banhoPet('Siames', 'Completo')
+
+console.log(banhoPet('Siames', 'Completo'))
+
+console.log(tosaPet('Perca', 'Nice'))
+
+console.log(alimentacaoPet('Felipi', 'Gostosa'))
--- 2 TRIMESTRE/modulo/mod02-ESM/mod-gatos.js
+++ 2 TRIMESTRE/modulo/mod02-ESM/mod-gatos.js
@@ -1,13 +1,13 @@
-let racaViraLata = 'Vira-Lata'
+
 
 export function banhoPet(racaGato, tipoBanho){
-
+    return `Gato ${racaGato} fez um banho do tipo ${tipoBanho}`
 }
 
 export function tosaPet(racaGato, tipotosa){
-    
+    return `Gato ${racaGato} fez uma tosa do tipo ${tipotosa}`
 }
 
 export function alimentacaoPet(racaGato, tipoRacao){
-    
+    return `Gato ${racaGato} comeu uma ração ${tipoRacao}`
 }
