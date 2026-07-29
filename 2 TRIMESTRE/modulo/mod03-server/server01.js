6bf27cb53caa6015850ace44e90a0684a3a7c3b4
Matheus-2313 <matheus.gomes.castilho@escola.pr.gov.br>
2026-07-29 11:37:38 +0000

    html aura e vamo aeee

--- 2 TRIMESTRE/modulo/mod03-server/server01.js
+++ 2 TRIMESTRE/modulo/mod03-server/server01.js
@@ -2,10 +2,17 @@ const http = require('node:http')
 const porta = 8004
 
 const server = http.createServer((req, res)=>{
-    res.statusCode = '201'
-    //res.setHeader('Content-Type', 'text/plain')
-    res.setHeader('Content-Type', 'text/html', 'charset=utf-8')
-    res.end('<h3>HTML Rocks!</h3>')
+    const novaUrl = new URL(req.url, `http://${req.headers.host}`)
+    const caminhoUrl = novaUrl.pathname
+    if(caminhoUrl === '/'){
+        res.statusCode = '201'
+        res.setHeader('Content-type', 'text/html; charset=utf-8')
+        res.end('<h3>HTML Rocks!</h3>')
+    }else{
+    res.statusCode = '401'
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.end('<h3>401 Não autorizado</h3>')
+    }
 })
 
 server.listen(porta, ()=>{
